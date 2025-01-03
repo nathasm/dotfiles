f45d74d50dd09da1f479483ca4647db8e2ff045b
Nathan Smith <nathan.smith@thesystech.com>
2025-01-03 15:54:09 -0500

    Update dotfiles

--- run_once_install_lunarvim.sh
+++ run_once_install_lunarvim.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-set -x
 
-bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/master/utils/installer/install.sh)
+if [ ! -d "$HOME/.config/lunarvim" ]; then
+   echo "Getting LunarVim"
+   bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/master/utils/installer/install.sh)
+else
+   echo ".config/lunarvim already found, skipping."
+fi
